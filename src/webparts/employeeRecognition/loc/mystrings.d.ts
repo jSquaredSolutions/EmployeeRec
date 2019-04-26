412a216374f425c39ab0e8dc4bab75ebc125c05f
jsquaredSolutions <info@jsquaredsolutions.net>
2019-04-26 16:11:11 -0400

    Init, I just ran yo generator

---
+++ src/webparts/employeeRecognition/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IEmployeeRecognitionWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'EmployeeRecognitionWebPartStrings' {
+  const strings: IEmployeeRecognitionWebPartStrings;
+  export = strings;
+}
